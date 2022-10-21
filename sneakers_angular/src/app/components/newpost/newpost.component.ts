ed6a5151af43ace42ac19e86fad9d2bbf067e3ef
BaironSanchezBit <baironsteven18@gmail.com>
2022-10-20 19:22:41 -0500

    update

--- sneakers_angular/src/app/components/newpost/newpost.component.ts
+++ sneakers_angular/src/app/components/newpost/newpost.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Post } from "../../models/post";
+import Swal from "sweetalert2";
+
 
 @Component({
   selector: 'app-newpost',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewpostComponent implements OnInit {
 
-  constructor() { }
+  postForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.postForm = this.fb.group({
+      tittleArticle: ['', [Validators.required]],
+      post: ['', [Validators.required]],
+      img: ['', [Validators.required]],
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  crearPost() {
+    console.log(this.postForm);
+    const Swal = require('sweetalert2');
+
+    const POST: Post = {
+      tittleArticle: this.postForm.get('tittleArticle')?.value,
+      post: this.postForm.get('post')?.value,
+      img: this.postForm.get('img')?.value
+    }
+
+    console.log(POST)
+    this.router.navigate(['/newpost']);
+    Swal.fire({
+      title: 'Exito!',
+      text: 'La publicacion se realizó correctamente',
+      icon: 'success',
+      confirmButtonText: 'Vale'
+    })
   }
 
 }
--- sneakers_express/index.js
+++ sneakers_express/index.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use('/api/usuario', require('./routes/usuario'));
+app.use('/api/post', require('./routes/post'));
 
 //Creacion del servidor para ejecutarlo en http://localhost:4000/
 app.listen(4000, () => {
