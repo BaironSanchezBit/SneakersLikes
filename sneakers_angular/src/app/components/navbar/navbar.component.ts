8ecee2fa7c8e7218c8114fcf23532f13c8a23b7e
BaironSanchezBit <baironsteven18@gmail.com>
2022-10-22 16:09:40 -0500

    update

--- sneakers_angular/src/app/components/navbar/navbar.component.ts
+++ sneakers_angular/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,8 +13,19 @@ import Swal from 'sweetalert2';
 export class NavbarComponent implements OnInit {
 
 
-  constructor() { }
+  usuarioForm: FormGroup;
 
+  listUsuario: Usuario[] = [];
+
+  constructor(private fb: FormBuilder, private router: Router, private _usuarioService: UsuarioService) {
+    this.usuarioForm = this.fb.group({
+      fullName: ['', [Validators.required]],
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      vereficarClave: ['', [Validators.required]]
+    })
+  }
   ngOnInit(): void {
   }
 
@@ -23,6 +38,37 @@ export class NavbarComponent implements OnInit {
     })
   }
   
+  crearUsuario() {
+
+    if (this.usuarioForm.get('password')?.value != this.usuarioForm.get('vereficarClave')?.value) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Las contraseñas no coinciden',
+      })
+
+    } else {
+
+      const USUARIO: Usuario = {
+        fullName: this.usuarioForm.get('fullName')?.value,
+        username: this.usuarioForm.get('username')?.value,
+        password: this.usuarioForm.get('password')?.value,
+        email: this.usuarioForm.get('email')?.value
+      }
+
+      this._usuarioService.postUsuario(USUARIO).subscribe(data => {
+        this.router.navigate(['/']);
+        Swal.fire({
+          title: 'Exito',
+          text: 'El registro se realizó correctamente',
+          icon: 'success',
+          confirmButtonText: 'Vale'
+        })
+      }, error =>{
+        console.log(error);
+      })
+    }
+  }
 }
 
 
