462683a31456281ed354cd53f25a72ccde1003cd
Carlos Hernández <calichenew@gmail.com>
2022-10-24 23:04:19 -0500

    merge carloV + Master

--- sneakers_express/routes/post.js
+++ sneakers_express/routes/post.js
@@ -1,10 +1,14 @@
 //Rutas para producto
 const express = require('express'); //Importamos el servicio de express
 const router = express.Router(); //Llamamos para controlar las rutas junto con express
-const usuarioController = require('../controllers/postController')
+const postController = require('../controllers/postController')
 
 //Configuramos la ruta usando el controlador
-router.post('/', postController.crearUsuario);
-router.get('/', postController.obtenerUsuario)
+router.post('/crear-post', postController.crearPost);
+router.put('/update-post/:id', postController.actualizarPost);
+router.delete('/borrar-post/:id' , postController.borrarPost)
+router.get('/publication', postController.obtenerPost);
+router.get('/publication/:id', postController.obtenerPostEspecifico);
 
-module.exports = router //Exportamos el modulo de rutas
+
+module.exports = router //Exportamos el modulo de rutas 
